96cd5a2d65ca66ca355af352657ec5fcd7e32a9b
Jonathan Flores <hiruma5436@gmail.com>
2024-08-09 17:29:19 -0600

    archivo

--- webpersonal/settings/prod.py
+++ webpersonal/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'https://web-production-b610.up.railway.app/']
+ALLOWED_HOSTS = ['localhost', 'web-production-b610.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -13,3 +13,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+#Media file
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
