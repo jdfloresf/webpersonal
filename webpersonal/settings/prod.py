b1b96650394d38d10fa1f3742a31a4995e30c82c
Jonathan Flores <hiruma5436@gmail.com>
2024-08-09 19:17:09 -0600

    postgres

--- webpersonal/settings/prod.py
+++ webpersonal/settings/prod.py
@@ -28,7 +28,7 @@ STATIC_ROOT = BASE_DIR /  'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-CSRF_TRUSTED_ORIGINS = ['http://*', 'https://web-production-b610.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://web-production-b610.up.railway.app']
 
 #Media file
 
