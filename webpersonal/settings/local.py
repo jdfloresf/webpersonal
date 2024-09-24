5d2676f34b4e7630a4a44c3f69baf64f71289234
Jonathan Flores <hiruma5436@gmail.com>
2024-09-24 17:58:03 -0600

    new domain

--- webpersonal/settings/local.py
+++ webpersonal/settings/local.py
@@ -31,9 +31,9 @@ API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'tu_cloud_name',
-    'API_KEY': 'tu_api_key',
-    'API_SECRET': 'tu_api_secret',
+    'CLOUD_NAME': 'CLOUD_NAME',
+    'API_KEY': 'API_KEY',
+    'API_SECRET': 'API_SECRET',
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
