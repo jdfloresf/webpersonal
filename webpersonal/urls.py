e7e2cd6d1cecdf95cc404d2425491ab0724e108f
Jonathan Flores <hiruma5436@gmail.com>
2024-10-01 14:15:12 -0600

    project detail

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('about-me/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
-    path('', include('portfolio.urls')),
+    path('portfolio/', include('portfolio.urls')),
 ]
 
 if settings.DEBUG:
