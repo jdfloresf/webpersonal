0acfe5876b281fe69a8ee1f288f6ae811d376fc4
Jonathan Flores <hiruma5436@gmail.com>
2024-10-01 14:08:37 -0600

    project detail

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,6 @@ from .views import *
 app_name = 'portfolio_app'
 
 urlpatterns = [
-    path('portfolio/', ProjectListView.as_view(), name='portfolio'),
+    path('', ProjectListView.as_view(), name='portfolio'),
     path('portfolio/<int:pk>/', ProjectDetailView.as_view(), name='portfolio_detail')
 ]
