639e9698e7988d716feeadfff97e790590b897eb
Jonathan Flores <hiruma5436@gmail.com>
2024-10-02 22:04:36 -0600

    slug field added

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,8 +7,8 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     descrption = models.TextField(verbose_name="Descripción")
     image = CloudinaryField('image', blank=True)
-    created = models.DateField(verbose_name="Fecha de creación")
-    updated =   models.DateField(verbose_name="Fecha de edición")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated =   models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
     slug = models.SlugField(editable=False, max_length=300)
 
     class Meta:
@@ -21,7 +21,5 @@ class Project(models.Model):
     
     def save(self, *args, **kwargs):
         slug_unique = '%s' % (self.title)
-
         self.slug = slugify(slug_unique)
-
         super(Project, self).save(*args, **kwargs)
