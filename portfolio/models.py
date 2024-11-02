de75e2c152cb926b092ae02206356392eaa19768
Jonathan Flores <hiruma5436@gmail.com>
2024-11-02 14:07:04 -0600

    RichTextField added

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 from cloudinary.models import CloudinaryField
-from ckeditor.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     descrption = models.TextField(verbose_name="Descripción")
-    content = RichTextUploadingField('contenido')
+    content = RichTextField('contenido')
     image = CloudinaryField('image', blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated =   models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
